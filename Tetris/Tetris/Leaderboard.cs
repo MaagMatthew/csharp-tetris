ebed794cb7ccf2008193995b80b0edd1f2553701
Sormokah <thestevencrawford@gmail.com>
2019-08-26 20:23:53 -0600

    Added Leaderboard

--- Tetris/Tetris/Leaderboard.cs
+++ Tetris/Tetris/Leaderboard.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 
 namespace Tetris
 {
@@ -10,24 +13,23 @@ namespace Tetris
     {
         static Leaderboard()
         {
-
+            Deserialize();
         }
 
         private readonly static string FileLocation = "../../leaderboard.lbd";
 
-        public static Dictionary<string, int> MyProperty { get; set; }
+        public static Dictionary<string, int> LeaderStored { get; set; }
 
+
+        private static Dictionary<string, int> leaderStored = new Dictionary<string, int>();
         private static void Serialize()
         {
-            
+            File.WriteAllText(FileLocation, JsonConvert.SerializeObject(LeaderStored));
         }
 
         private static void Deserialize()
         {
-
+            LeaderStored = JsonConvert.DeserializeObject<Dictionary<string, int>>(File.ReadAllText(FileLocation));
         }
-
-
-
     }
 }
