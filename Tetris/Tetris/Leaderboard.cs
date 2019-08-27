30e55bdc5183265758e3f5cdc5324326cc2799ab
Sormokah <thestevencrawford@gmail.com>
2019-08-26 19:26:45 -0600

    Leaderboard

---
+++ Tetris/Tetris/Leaderboard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    static class Leaderboard
+    {
+        static Leaderboard()
+        {
+
+        }
+
+        private readonly static string FileLocation = "../../leaderboard.lbd";
+
+        public static Dictionary<string, int> MyProperty { get; set; }
+
+        private static void Serialize()
+        {
+            
+        }
+
+        private static void Deserialize()
+        {
+
+        }
+
+
+
+    }
+}
